2de66fb9a6a0b55b4334a9586608c678a540b81a
Jiayu Yi <yijiayu@gmail.com>
2020-08-09 22:47:27 +0800

    Reorganise code

--- cmd/parlor/main.go
+++ cmd/parlor/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"encoding/base64"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"math/rand"
 	"net/http"
@@ -12,10 +9,8 @@ import (
 	"sync"
 
 	"github.com/gin-contrib/sessions"
-	sredis "github.com/gin-contrib/sessions/redis"
+	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
-
-	"github.com/yi-jiayu/mahjong.go"
 )
 
 type PlayerRegistry struct {
@@ -29,65 +24,6 @@ func (r *PlayerRegistry) GetName(id string) string {
 	return r.Names[id]
 }
 
-func newRoomID() string {
-	charset := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	return string([]byte{
-		charset[rand.Intn(len(charset))],
-		charset[rand.Intn(len(charset))],
-		charset[rand.Intn(len(charset))],
-		charset[rand.Intn(len(charset))],
-	})
-}
-
-type RoomRepository interface {
-	Insert(room *Room) (string, error)
-	Save(id string, room *Room) error
-	Get(id string) (*Room, error)
-}
-
-type InMemoryRoomRepository struct {
-	sync.RWMutex
-	rooms map[string]*Room
-}
-
-func NewInMemoryRoomRepository() *InMemoryRoomRepository {
-	return &InMemoryRoomRepository{
-		rooms: map[string]*Room{},
-	}
-}
-
-func (r *InMemoryRoomRepository) Insert(room *Room) (string, error) {
-	r.Lock()
-	defer r.Unlock()
-	for {
-		id := newRoomID()
-		if _, ok := r.rooms[id]; ok {
-			continue
-		}
-		r.rooms[id] = room
-		return id, nil
-	}
-}
-
-func (r *InMemoryRoomRepository) Save(id string, room *Room) error {
-	r.Lock()
-	defer r.Unlock()
-	r.rooms[id] = room
-	return nil
-}
-
-func (r *InMemoryRoomRepository) Get(id string) (*Room, error) {
-	r.RLock()
-	defer r.RUnlock()
-	return r.rooms[id], nil
-}
-
-type Action struct {
-	Nonce int      `json:"nonce"`
-	Type  string   `json:"type"`
-	Tiles []string `json:"tiles"`
-}
-
 var (
 	playerRegistry = &PlayerRegistry{
 		Names: map[string]string{},
@@ -95,149 +31,15 @@ var (
 	roomRepository RoomRepository = NewInMemoryRoomRepository()
 )
 
-const (
-	PhaseLobby = iota
-	PhaseInProgress
-)
-
-type Room struct {
-	Nonce   int
-	Phase   int
-	Players []string
-	Round   *mahjong.Round
-
-	sync.Mutex
-	clients map[chan string]struct{}
-}
-
-func (r *Room) MarshalJSON() ([]byte, error) {
-	players := make([]string, len(r.Players))
-	for i, playerID := range r.Players {
-		players[i] = playerRegistry.GetName(playerID)
-	}
-	return json.Marshal(struct {
-		Nonce   int            `json:"nonce"`
-		Phase   int            `json:"phase"`
-		Players []string       `json:"players"`
-		Round   *mahjong.Round `json:"round"`
-	}{
-		Nonce:   r.Nonce,
-		Phase:   r.Phase,
-		Players: players,
-		Round:   r.Round,
-	})
-}
-
 func newSessionID() string {
 	b := make([]byte, 32)
 	rand.Read(b)
 	return base64.StdEncoding.EncodeToString(b)
 }
 
-func NewRoom(host string) *Room {
-	return &Room{
-		Players: []string{host},
-		clients: map[chan string]struct{}{},
-	}
-}
-
-func (r *Room) addClient(c chan string) {
-	r.Lock()
-	defer r.Unlock()
-
-	r.clients[c] = struct{}{}
-	var b bytes.Buffer
-	json.NewEncoder(&b).Encode(r)
-	c <- b.String()
-}
-
-func (r *Room) AddPlayer(id string) error {
-	r.Lock()
-	defer r.Unlock()
-	if len(r.Players) == 4 {
-		return errors.New("room full")
-	}
-	for _, p := range r.Players {
-		if p == id {
-			return errors.New("already joined")
-		}
-	}
-	r.Players = append(r.Players, id)
-	return nil
-}
-
-func (r *Room) removeClient(c chan string) {
-	r.Lock()
-	defer r.Unlock()
-
-	delete(r.clients, c)
-}
-
-func (r *Room) broadcast() {
-	r.Lock()
-	defer r.Unlock()
-
-	var b bytes.Buffer
-	json.NewEncoder(&b).Encode(r)
-	for c := range r.clients {
-		c <- b.String()
-	}
-}
-
-func (r *Room) startRound() error {
-	if len(r.Players) < 4 {
-		return errors.New("not enough players")
-	}
-	r.Round = mahjong.NewRound(0, mahjong.DirectionEast)
-	r.Phase = PhaseInProgress
-	r.Nonce++
-	return nil
-}
-
-func (r *Room) HandleAction(playerID string, action Action) error {
-	r.Lock()
-	defer r.Unlock()
-	seat := -1
-	for i, p := range r.Players {
-		if p == playerID {
-			seat = i
-			break
-		}
-	}
-	if seat == -1 {
-		return errors.New("player not in room")
-	}
-	if action.Nonce != r.Nonce {
-		return errors.New("invalid nonce")
-	}
-	switch action.Type {
-	case "start":
-		return r.startRound()
-	case "discard":
-		if len(action.Tiles) < 0 {
-			return errors.New("not enough tiles")
-		}
-		return r.Round.Discard(seat, action.Tiles[0])
-	case "draw":
-		return r.Round.Draw(seat)
-	case "chow":
-		if len(action.Tiles) < 2 {
-			return errors.New("not enough tiles")
-		}
-		return r.Round.Chow(seat, action.Tiles[0], action.Tiles[1])
-	case "peng":
-		if len(action.Tiles) < 1 {
-			return errors.New("not enough tiles")
-		}
-		return r.Round.Peng(seat, action.Tiles[0])
-	default:
-		return errors.New("invalid action")
-	}
-}
-
 func main() {
 	r := gin.Default()
-	store, err := sredis.NewStore(10, "tcp", "localhost:6379", "", []byte("secret"))
+	store, err := redis.NewStore(10, "tcp", "localhost:6379", "", []byte("secret"))
 	if err != nil {
 		panic(err)
 	}
